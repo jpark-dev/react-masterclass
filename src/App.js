1333be513d4fb6c44cdd4963aa5c6af065217287
Jason Park <zeipar@gmail.com>
2022-10-01 17:05:34 -0700

    Practice extending existing styled-components.

--- src/App.js
+++ src/App.js
@@ -4,14 +4,8 @@ const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
-  width: 100px;
-  height: 100px;
-`;
-
-const BoxTwo = styled.div`
-  background-color: tomato;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
@@ -20,13 +14,17 @@ const Text = styled.span`
   color: white;
 `;
 
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
+
 function App() {
   return (
     <Father>
-      <BoxOne>
+      <Box bgColor="teal">
         <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
+      </Box>
+      <Circle bgColor="tomato" />
     </Father>
   )
 }
