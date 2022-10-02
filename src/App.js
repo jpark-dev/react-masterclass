4ac9b785890849340b2f63f24238c3474e22478f
Jason Park <zeipar@gmail.com>
2022-10-01 17:08:56 -0700

    Practice using 'as' to change the tag name.

--- src/App.js
+++ src/App.js
@@ -4,27 +4,19 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-
-const Text = styled.span`
+const Btn = styled.button`
   color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
 `;
 
-const Circle = styled(Box)`
-  border-radius: 50px;
-`;
 
 function App() {
   return (
     <Father>
-      <Box bgColor="teal">
-        <Text>Hello</Text>
-      </Box>
-      <Circle bgColor="tomato" />
+      <Btn>Login</Btn>
+      <Btn as="a" href="https://www.google.com">Login</Btn>
     </Father>
   )
 }
