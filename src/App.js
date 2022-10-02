eea880d3981ba255c7cf900b1e7ac57b0ec7b55a
Jason Park <zeipar@gmail.com>
2022-10-01 17:21:28 -0700

    Practiced using keyframe animations, and pseudo selectors.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const Wrapper = styled.div`
   display: flex;
 `;
 
+const animation = keyframes`
+  0% {
+    transform:rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    transform:rotate(360deg);
+    border-radius: 100px;
+  }
+  100% {
+    transform:rotate(0deg);
+    border-radius: 0px;
+  }
+`;
+
 const Box = styled.div`
   height: 200px;
-  weight: 200px;
+  width: 200px;
   background-color: tomato;
+  animation: ${animation} 3s linear infinite;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  span {
+    font-size: 50px;
+    &:hover {
+      font-size: 70px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box />
+      <Box>
+        <span>😍</span>
+      </Box>
     </Wrapper>
   )
 }
