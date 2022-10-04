c27d8bcc01c98a325002abd1382c168c536558fb
Jason Park <zeipar@gmail.com>
2022-10-03 22:24:52 -0700

    Practiced typescript for form/input events.

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,25 @@
-import styled, { keyframes } from "styled-components";
-import Circle from "./Circle";
-
-const Wrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props) => props.theme.backgroundColor}
-`;
-
-const animation = keyframes`
-  0% {
-    transform:rotate(0deg);
-    border-radius: 0px;
-  }
-  50% {
-    transform:rotate(360deg);
-    border-radius: 100px;
-  }
-  100% {
-    transform:rotate(0deg);
-    border-radius: 0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 50px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  animation: ${animation} 3s linear infinite;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  ${Emoji} {
-    &:hover {
-      font-size: 150px;
-    }
-  }
-`;
-
-const Text = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
+import React, { useState } from "react";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setUsername(value);
+  };
+  const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(`submit ${username}`);
+  };
+
   return (
-    <Wrapper>
-      <Box>
-        <Text>Welcome</Text>
-        <Emoji>😍</Emoji>
-      </Box>
-      <div>
-        <Circle bgColor="teal"/>
-        <Circle text="text exists" bgColor="blue" borderColor="red" />
-      </div>
-    </Wrapper>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={username} onChange={onChange} type="text" placeholder="username" />
+        <button>Log in</button>
+      </form>
+    </div>
   )
 }
 
