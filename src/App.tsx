d27fde7ea4b7f29bb71b6295c4381941fd402ef2
Jason Park <zeipar@gmail.com>
2022-10-04 23:20:36 -0700

    Created a simple router with a page receives a param.

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,8 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-
-const Wrapper = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-  color: ${(props) => props.theme.textColor};
-  height: 300px;
-  width: 500px;
-`;
-
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
-const Btn = styled.button`
-  color: ${(props) => props.theme.btnColor};
-`;
+import Router from "./Router";
 
 function App() {
-  const [username, setUsername] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setUsername(value);
-  };
-  const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(`submit ${username}`);
-  };
-
   return (
-    <Wrapper>
-      <H1>
-        hwll
-      </H1>
-      <form onSubmit={onSubmit}>
-        <input value={username} onChange={onChange} type="text" placeholder="username" />
-        <Btn>Log in</Btn>
-      </form>
-    </Wrapper>
+    <Router />
   )
 }
 
---
+++ src/Router.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Coin from "./routes/Coin";
+import Coins from "./routes/Coins";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/:coinID" element={<Coin/>}/>
+        <Route path="/" element={<Coins/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Router;
