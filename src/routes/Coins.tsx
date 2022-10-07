095343d03eb75d22cefb4ad78a5d2b22784e58a6
Jason Park <zeipar@gmail.com>
2022-10-07 01:43:19 -0700

    Added a simple darktheme toggling.

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -3,11 +3,15 @@ import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { fetchCoins } from "../api";
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import { useState } from "react";
 
-const Container = styled.div`
+const Container = styled.div<ITheme>`
   padding: 0px 30px;
   max-width: 480px;
   margin: 0 auto;
+  background-color: ${props => props.darkTheme ? props.theme.bgColor : props.theme.textColor};
 `;
 
 const Header = styled.header`
@@ -20,9 +24,9 @@ const Header = styled.header`
 
 const CoinsList = styled.ul``;
 
-const Coin = styled.li`
+const Coin = styled.li<ITheme>`
   background-color: white;
-  color:${props => props.theme.bgColor};
+  color:${props => props.darkTheme ? props.theme.bgColor : props.theme.textColor}};
   margin-bottom: 10px;
   border-radius: 20px;
   a {
@@ -33,14 +37,14 @@ const Coin = styled.li`
   }
   &:hover {
     a {
-      color:${props => props.theme.accentColor};
+      color:${props => props.darkTheme ? props.theme.accentColor : 'red'};
     }
   }
 `;
 
-const Title = styled.h1`
+const Title = styled.h1<ITheme>`
   font-size: 48px;
-  color: ${props => props.theme.accentColor};
+  color: ${props => props.darkTheme ? props.theme.accentColor : 'red'};
 `;
 
 const Loader = styled.span`
@@ -63,27 +67,37 @@ interface ICoin {
   is_active: boolean,
   type: string,
 }
+interface ITheme {
+  darkTheme: boolean;
+}
 
 function Coins() {
+  const [darkTheme, setDarkTheme] = useState(false);
   const { isLoading, data } = useQuery<ICoin[]>("allCoins", fetchCoins);
 
+  const toggleDarkMode = () => {
+    setDarkTheme(!darkTheme)
+  };
+
+
   return (
-    <Container>
+    <Container darkTheme={darkTheme}>
       <Helmet>
         <title>Coin</title>
       </Helmet>
       <Header>
-        <Title>Coin</Title>
+        <Title darkTheme={darkTheme}>Coin</Title>
+        {darkTheme ? <DarkModeIcon color="primary" onClick={toggleDarkMode} /> : <LightModeIcon color="primary" onClick={toggleDarkMode} />}
       </Header>
       {isLoading ? (
         <Loader>Loading...</Loader>
       ) : (
         <CoinsList>
           {data?.slice(0, 100).map(coin =>
-            <Coin key={coin.id}>
+            <Coin key={coin.id} darkTheme={darkTheme}>
               <Link
                 to={`/${coin.id}`}
-                state={{'coin': coin.name}}
+                state={{'coin': coin.name, darkTheme}}
               >
                 <Img src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`} alt={coin.id}/>
                 {coin.name} &rarr;
