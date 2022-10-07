9f7594685a239b7b8df86aa23308ef941e73e667
Jason Park <zeipar@gmail.com>
2022-10-06 22:50:08 -0700

    Installed react-query and refactored Coins component using react-query.

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { fetchCoins } from "../api";
 
 const Container = styled.div`
   padding: 0px 30px;
@@ -52,7 +53,7 @@ const Img = styled.img`
   margin-right: 10px;
 `;
 
-interface CoinInterface {
+interface ICoin {
   id: string,
   name: string,
   symbol: string,
@@ -63,27 +64,18 @@ interface CoinInterface {
 }
 
 function Coins() {
-  const [coins, setCoins] = useState<CoinInterface[]>([]);
-  const [loading, setLoading] = useState(true);
+  const { isLoading, data } = useQuery<ICoin[]>("allCoins", fetchCoins);
 
-  useEffect(() => {
-    (async() => {
-      const res = await fetch('https://api.coinpaprika.com/v1/coins');
-      const json = await res.json();
-      setCoins(json.slice(0, 100));
-      setLoading(false);
-    })();
-  }, [])
   return (
     <Container>
       <Header>
         <Title>Coin</Title>
       </Header>
-      {loading ? (
+      {isLoading ? (
         <Loader>Loading...</Loader>
       ) : (
         <CoinsList>
-          {coins.map(coin =>
+          {data?.slice(0, 100).map(coin =>
             <Coin key={coin.id}>
               <Link
                 to={`/${coin.id}`}
